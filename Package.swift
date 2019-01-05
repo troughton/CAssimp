9cd3f0e558fd692ea2af73cd462318fcda6f0484
Thomas Roughton <t.roughton@me.com>
2019-01-05 21:51:52 +1300

    Update to tools v4

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "CAssimp",
     pkgConfig: "assimp",
     providers: [
-      .Brew("assimp")
+      .brew(["assimp"])
     ]
 )
